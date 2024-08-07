0930db51e1864c31db28624e5cef7c53b1db0418
Emily Li <emily1998ljy@gmail.com>
2024-08-07 11:24:49 -0700

    refactor register form

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,19 @@
+import '@radix-ui/themes/styles.css';
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import "./globals.css";
+import { Theme } from '@radix-ui/themes';
 
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
         <head />
         <body className="">
-          <Navbar/>
-          {children}
-          <Footer/>
+          <Theme>
+            <Navbar/>
+            {children}
+            <Footer/>
+          </Theme>
         </body>
       </html>
     );
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,53 +1,59 @@
-'use client'
-import { useState } from 'react'
+'use client';
+import React from 'react'
 import Layout from "../[components]/Layout/Layout"
+import { TextField, Button } from '@radix-ui/themes'
+import { useForm } from 'react-hook-form'
+
 
 export default function Register(){
-    
-    const [person, setPerson] = useState({
-        name: "",
-        age: ""
-    });
+    const {register, handleSubmit, formState: {errors}} = useForm();
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setPerson({
-            ...person,
-            [name]: value,
-        });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
     return (
         <Layout>
-            <form onSubmit={handleSubmit} className="max-w-md mx-auto p-4 border border-gray-300 rounded-md shadow-md">
-                <div className="mb-4">
-                    <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={person.name}
-                        onChange={handleChange}
-                        className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="age" className="block text-sm font-medium text-gray-700">Age</label>
-                    <input
-                        type="number"
-                        id="age"
-                        name="age"
-                        value={person.age}
-                        onChange={handleChange}
-                        className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200"
-                    />
-                </div>
-                <button type="submit" className="w-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">Register</button>
+            <form 
+                className="max-w-md space-y-3"
+                onSubmit={handleSubmit((data) => console.log(data))}>
+                <TextField.Root 
+                    placeholder='Username' 
+                    {...register('username', {
+                        required: "Username is required",
+                    })} 
+                    type="text"></TextField.Root>
+                    {errors.username && (
+                        <div className="text-red-500">{errors.username.message}</div>
+                    )}
+                <TextField.Root 
+                    placeholder='Password' 
+                    {...register('password', {
+                        required: "Password is required",
+                        minLength: {
+                            value: 8,
+                            message: "Password must have at least 8 characters"
+                        },
+                    })} 
+                    type="password"></TextField.Root>
+                    {errors.password && (
+                        <div className="text-red-500">{errors.password.message}</div>
+                    )}
+                <TextField.Root 
+                    placeholder='Email' 
+                    {...register('email', {
+                        required: "Email is required",
+                        validate: (value) => {
+                            if(!value.includes("@")){
+                                return "Email must include @";
+                            }
+                            return true;
+                        },
+                    })} 
+                    type="text"></TextField.Root>
+                    {errors.email && (
+                        <div className="text-red-500">{errors.email.message}</div>
+                    )}
+                <Button>Register</Button>
             </form>
+            
         </Layout>
     )
-     
+    
 }
