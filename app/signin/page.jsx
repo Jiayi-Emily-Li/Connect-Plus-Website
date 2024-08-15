90f4ccf730e3a9b8e92ef7d4940a178258a4e753
Emily Li <emily1998ljy@gmail.com>
2024-08-15 13:28:22 -0700

    refactor sign in

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -4,20 +4,47 @@ import Layout from "../[components]/Layout/Layout"
 import Link from "next/link";
 import { TextField, Button, Card, Container, Heading, Flex } from '@radix-ui/themes'
 import { useForm } from 'react-hook-form'
-import { ref, push } from 'firebase/database';
+import { ref, push, query, get, orderByChild, equalTo } from 'firebase/database';
 import { database } from '../firebaseConfig';
 
 export default function Signin() {
-    const {register, handleSubmit, formState: {errors}} = useForm();
+    const {register, handleSubmit, formState: {errors}, reset} = useForm();
 
     const onSubmit = async (data) => {
         try{
             const reference = ref(database, 'users'); 
-            await push(reference, data);
-            reset(); 
+            //const emailQuery = query(reference, orderByChild('email'), equalTo(data.email));
+            const snapshot = await get(reference);
+
+            if(snapshot.exists()){
+                const users = snapshot.val();
+                let userFound = null;
+                Object.keys(users).forEach(userId => {
+                    if (users[userId].email === data.email) {
+                      userFound = users[userId];
+                    }
+                  });
+
+                if(userFound){
+                    const isPasswordValid = (data.password === userFound.password) ? true : false;
+                    if(isPasswordValid){
+                        console.log('User signed in successfully:', userFound);
+                        alert('Signed in successfully.');
+                    }
+                    else{
+                        alert('Invalid password.');
+                    }
+                }
+                else{
+                    alert('User not found.');
+                }
+                
+            }
+            
         } catch (error){
             console.error(error);
         }
+        reset();
     };
 
     return (
@@ -36,13 +63,13 @@ export default function Signin() {
                             onSubmit={handleSubmit(onSubmit)}>
                             <TextField.Root 
                                 size="3"
-                                placeholder='Username/Email' 
-                                {...register('username', {
-                                    required: "Username is required",
+                                placeholder='Email' 
+                                {...register('email', {
+                                    required: "Email is required",
                                 })} 
                                 type="text"></TextField.Root>
-                                {errors.username && (
-                                    <div className="text-red-500">{errors.username.message}</div>
+                                {errors.email && (
+                                    <div className="text-red-500">{errors.email.message}</div>
                                 )}
                             <TextField.Root 
                                 size="3"
@@ -63,7 +90,7 @@ export default function Signin() {
                                 <Link href="/register" passHref>
                                     <Button variant="soft" size="3">Create an account</Button>
                                 </Link>
-                                <Button variant="classic" size="3">Sign in</Button>
+                                <Button variant="classic" size="3" type="submit">Sign in</Button>
                             </Flex>
                         </form>
                     {/* </Card> */}
