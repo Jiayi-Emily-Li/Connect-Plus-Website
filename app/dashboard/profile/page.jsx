912eb6bdc44f9e70782551318417805360f66a73
Emily Li <emily1998ljy@gmail.com>
2024-11-08 10:16:04 -0800

    populate data

--- app/dashboard/profile/page.jsx
+++ app/dashboard/profile/page.jsx
@@ -1,9 +1,9 @@
 'use client';
 import { TextField, Flex, Button, TextArea, DropdownMenu} from '@radix-ui/themes';
 import { useForm } from 'react-hook-form';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import  Cookies from 'js-cookie';
-import { doc, updateDoc} from 'firebase/firestore';
+import { doc, getDoc, updateDoc} from 'firebase/firestore';
 import { database } from '../../firebaseConfig';
 
 export default function profile(){
@@ -11,14 +11,54 @@ export default function profile(){
     const dropDownOptions = ["A1 (Beginner)", "A2 (Elemantry)", "B1 (Intermediate)", 
                                 "B2 (Upper intermediate)", "C1 (Advanced)", "C2 (Proficient)"];
 
-    const [dropdown, setDropdown] = useState("CEFR English Level");
-
     //GET USER INFO
     const userID = Cookies.get('userID');
     const userDocRef = doc(database, 'users', userID);
 
+    const fetchUserData = async () => {
+        try{
+            const docSnapShot = await getDoc(userDocRef);
+            if(docSnapShot.exists()){
+                const userData = docSnapShot.data();
+                return userData;
+            }
+            else{
+                console.log("No user document");
+                return null;
+            }
+        } catch(error){
+            console.error(error);
+        }
+    }
+
+    
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [age, setAge] = useState("");
+    const [wechat, setWechat] = useState("");
+    const [level, setLevel] = useState("CEFR English Level");
+    const [goal, setGoal] = useState("");
+
+    useEffect(() => {
+        // Fetch the user data when the component mounts
+        fetchUserData().then((userData) => {
+          if (userData) {
+            setUsername(userData.username);
+            setEmail(userData.email);
+            setFirstname(userData.first_name);
+            setLastname(userData.last_name);
+            setAge(userData.age);
+            setWechat(userData.wechat_id);
+            setLevel(userData.cefr_level);
+            setGoal(userData.goal);
+          }
+        });
+      }, []);
+
     const handleSelect = async (option) => {
-        setDropdown(option); 
+        setLevel(option); 
         if(userID){
             await updateDoc(userDocRef, {
                 cefr_level: option,
@@ -61,6 +101,7 @@ export default function profile(){
                         size="3"
                         placeholder='Username'
                         disabled
+                        value={username}
                     >
                         
                     </TextField.Root>
@@ -68,6 +109,7 @@ export default function profile(){
                         size="3"
                         placeholder='Email'
                         disabled
+                        value={email}
                     >
                         
                     </TextField.Root>
@@ -80,7 +122,8 @@ export default function profile(){
                         {...register('firstname', {
                             required: "First Name is required",
                         })} 
-                        type="text"></TextField.Root>
+                        type="text"
+                        defaultValue={firstname}></TextField.Root>
                         {errors.firstname && (
                             <div className="text-red-500">{errors.firstname.message}</div>
                         )}
@@ -90,7 +133,8 @@ export default function profile(){
                         {...register('lastname', {
                             required: "Last Name is required",
                         })} 
-                        type="text"></TextField.Root>
+                        type="text"
+                        defaultValue={lastname}></TextField.Root>
                         {errors.lastname && (
                             <div className="text-red-500">{errors.lastname.message}</div>
                         )}
@@ -100,7 +144,8 @@ export default function profile(){
                         {...register('age', {
                             required: "Age is required",
                         })} 
-                        type="text"></TextField.Root>
+                        type="text"
+                        defaultValue={age}></TextField.Root>
                         {errors.age && (
                             <div className="text-red-500">{errors.age.message}</div>
                         )}
@@ -108,20 +153,21 @@ export default function profile(){
 
                 <div className="grid grid-cols-2 gap-4">
                     <TextField.Root 
-                            size="3"
-                            placeholder='WeChat ID' 
-                            {...register('wechat', {
-                                required: "WeChat ID is required",
-                            })} 
-                            type="text"></TextField.Root>
-                            {errors.wechat && (
-                                <div className="text-red-500">{errors.wechat.message}</div>
-                            )}
+                        size="3"
+                        placeholder='WeChat ID' 
+                        {...register('wechat', {
+                            required: "WeChat ID is required",
+                        })} 
+                        type="text"
+                        defaultValue={wechat}></TextField.Root>
+                        {errors.wechat && (
+                            <div className="text-red-500">{errors.wechat.message}</div>
+                        )}
 
                     <DropdownMenu.Root>
                         <DropdownMenu.Trigger>
                             <Button color="gray" variant="surface" size="3">
-                                {dropdown}
+                                {level}
                                 <DropdownMenu.TriggerIcon />
                             </Button>
                         </DropdownMenu.Trigger>
@@ -147,7 +193,8 @@ export default function profile(){
                         {...register('goal', {
                             required: "Study goal is required",
                         })} 
-                        type="text"></TextArea>
+                        type="text"
+                        defaultValue={goal}></TextArea>
                         {errors.goal && (
                              <div className="text-red-500">{errors.goal.message}</div>
                         )}
